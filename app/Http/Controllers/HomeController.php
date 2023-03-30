cfbfa8437c3e95145c01ed6ad25310692b149e70
imjad <imjad@exvivo.cloud>
2023-03-30 18:32:45 +0000

    tests are not expecting return,  bring back the old format.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,12 +9,12 @@ use Laravel\Lumen\Routing\Controller;
 
 final class HomeController extends Controller
 {
+    // Tests are not expecting return,  bring back the old format.
     public function home(): JsonResponse
     {
         return JsonResponse::create([
             'name' => config('app.name'),
-            'version' => config('aepg.version'),
-            'test' => 'hello world'
+            'version' => config('aepg.version')
         ]);
     }
 }
